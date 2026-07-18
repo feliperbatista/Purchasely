73f29665ecf8eca6d4373fb3748d5b1102798734
Felipe Batista <feliperodriguesbatista@outlook.com>
2026-07-18 20:36:53 -0300

    feat: add toast on supplier create and edit

--- frontend/src/pages/suppliers/SupplierDetailsPage.tsx
+++ frontend/src/pages/suppliers/SupplierDetailsPage.tsx
@@ -14,6 +14,7 @@ import { useState } from 'react';
 import DeleteDialog from '../../components/common/DeleteDialog';
 import SupplierProductModal from '../../components/suppliers/SupplierProductModal';
 import type { Product } from '../../types/product';
+import { toast } from 'sonner';
 
 export default function SupplierDetailsPage() {
   const { id } = useParams();
@@ -45,9 +46,19 @@ export default function SupplierDetailsPage() {
 
   const onSubmit = (data: SupplierFormData) => {
     if (supplier) {
-      update.mutate(data, { onSuccess: () => navigate('/suppliers') });
+      update.mutate(data, {
+        onSuccess: () => {
+          toast.success('Supplier successfully updated');
+          navigate('/suppliers');
+        },
+      });
     } else {
-      create.mutate(data, { onSuccess: () => navigate('/suppliers') });
+      create.mutate(data, {
+        onSuccess: (s) => {
+          toast.success('Supplier successfully created');
+          navigate(`/suppliers/${s.id}`);
+        },
+      });
     }
   };
 

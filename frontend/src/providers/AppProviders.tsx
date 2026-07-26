a3c8f770714da01391289a1d069f02da63ca49fa
Felipe Batista <feliperodriguesbatista@outlook.com>
2026-07-25 21:15:37 -0300

    chore: add tanstack dev tools

--- frontend/src/providers/AppProviders.tsx
+++ frontend/src/providers/AppProviders.tsx
@@ -1,12 +1,14 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '../lib/queryClient';
 import { Toaster } from 'sonner';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
       <Toaster position='bottom-right' richColors />
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 }

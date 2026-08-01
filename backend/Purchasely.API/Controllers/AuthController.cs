366ab27f8536322314e99adec655bc49b6a9a29d
Felipe Batista <feliperodriguesbatista@outlook.com>
2026-08-01 18:09:30 -0300

    refactor: change register to user controller

--- backend/Purchasely.API/Controllers/AuthController.cs
+++ backend/Purchasely.API/Controllers/AuthController.cs
@@ -1,10 +1,6 @@
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Org.BouncyCastle.Ocsp;
-using Purchasely.Application.DTOs;
-using Purchasely.Application.Features.Products.Commands;
-using Purchasely.Application.Features.Products.Queries;
 using Purchasely.Application.Features.Users.Commands;
 using Purchasely.Application.Features.Users.Queries;
 
@@ -17,18 +13,6 @@ public class AuthController(
     IWebHostEnvironment environment
     ) : ControllerBase
 {
-    [Authorize(Roles = "Admin")]
-    [HttpPost("register")]
-    public async Task<IActionResult> Register([FromBody] CreateUserCommand command, CancellationToken cancellationToken)
-    {
-        var result = await mediator.Send(command, cancellationToken);
-
-        if (!result.IsSuccess)
-            return StatusCode(result.StatusCode, result.Errors);
-
-        return Created("", result.Value);
-    }
-
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] LoginCommand command, CancellationToken cancellationToken)
     {

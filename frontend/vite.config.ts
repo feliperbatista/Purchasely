7538ea6c992c1991523bc4bc33a948e5e5791ba6
Felipe Batista <feliperodriguesbatista@outlook.com>
2026-08-01 22:32:31 -0300

    chore: change api ref in vite config to look at env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5206',
-        changeOrigin: true,
-      },
-      '/hubs': {
-        target: 'http://localhost:5206',
-        changeOrigin: true,
-        ws: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': '/src',
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+        '/hubs': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          ws: true,
+        },
+      },
+    },
+  };
 });
